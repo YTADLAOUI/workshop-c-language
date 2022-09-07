b2476e295e656aaac4c61646ad1bbae08ada6f16
youssef tadlaoui <usftadlaoui@gmail.com>
2022-09-07 17:11:18 +0100

    projet3

---
+++ week_01/les projets/Project3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	printf("un programme qui demande la distance en Mile et la transforme en km La formule");
+	float M,km;
+	printf("Entre la distance en Mile: ");
+	scanf("%f",&M);
+	km = M*1.609 ;
+	printf("la distance en km est :%f",km);
+	return 0;
+}
